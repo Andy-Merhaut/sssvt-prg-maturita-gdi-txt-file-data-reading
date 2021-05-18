5de9543c9e3105304cce2c48b7d0ca75e698e631
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-18 19:11:40 +0200

    Přidejte soubory projektu.

---
+++ MaturitaGDISloupovyGraf/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace MaturitaGDISloupovyGraf
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(1037, 601);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 601);
+            this.Controls.Add(this.MainPictureBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+    }
+}
+
---
+++ MaturitaGDISloupovyGraf/TextFile.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace MaturitaGDISloupovyGraf
+{
+    public class TextFile
+    {
+        public string PrvniRadek(string cestaKSouboru)
+        {
+            StreamReader reader = new StreamReader(cestaKSouboru);
+            return reader.ReadLine();
+        }
+
+        public void ZapisRadek(string cestaKSouboru, string upraveneNoveCislaGrafu)
+        {
+            StreamWriter writer = new StreamWriter(cestaKSouboru);
+            writer.WriteLine(upraveneNoveCislaGrafu);
+        }
+    }
+}
